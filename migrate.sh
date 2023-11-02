0e6503e39f03feff39a8e41f84e1496e6de9f4c1
prad6588 <73686322+prad6588@users.noreply.github.com>
2023-11-02 16:24:21 +0530

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
-set -ex
+set -x
 gh repo create angular --private
-git clone --bare https://oauth2:$ADO_TOKEN@dev.azure.com/CDEDevOps/CDEDevOps_Assets/_git/AngularApp
+git clone --bare https://oauth2:$ADO_TOKEN@dev.azure.com/CDEDevOps/CDEDevOps_Assets/_git/AngularApp || echo "Repo exists"
 cd AngularApp
 git push --mirror https://github.com/prad6588/AngularApp
 cd ..
