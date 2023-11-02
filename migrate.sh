b83373e2f52b0c92c309ac814e1171711c000d4f
prad6588 <73686322+prad6588@users.noreply.github.com>
2023-11-02 18:20:30 +0530

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -5,6 +5,6 @@ cd AngularApp.git
 git remote rm origin
 git remote add origin https://prad6588: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@github.com/prad6588/AngularApp.git
 git remote set-url origin https://prad6588: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@github.com/prad6588/AngularApp.git
-git push --mirror https://prad6588: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/AngularApp@github.com/prad6588/AngularApp.git
+git push --mirror https://prad6588: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@github.com/prad6588/AngularApp.git
 cd ..
 rm -rf AngularApp.git
