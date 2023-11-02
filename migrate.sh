b20932a35775c5c7ba3e6d95da262af2d65dcc1a
prad6588 <73686322+prad6588@users.noreply.github.com>
2023-11-02 17:37:31 +0530

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -2,6 +2,7 @@ set -x
 gh repo create AngularApp --private
 git clone --bare https://oauth2:$ADO_TOKEN@dev.azure.com/CDEDevOps/CDEDevOps_Assets/_git/AngularApp || echo "Repo exists"
 cd AngularApp.git
+git remote add origin https://prad6588:$GH_PAT@github.com/prad6588/AngularApp.git
 git remote set-url origin https://prad6588:$GH_PAT@github.com/prad6588/AngularApp.git
 git push --mirror https://github.com/prad6588/AngularApp.git
 cd ..
