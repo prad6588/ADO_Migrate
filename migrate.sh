791c2861ddce7f5200234682ac3acc236811d211
prad6588 <73686322+prad6588@users.noreply.github.com>
2023-11-03 19:13:10 +0530

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,18 @@
 set -x
+jq -c '.github_migration[]' "github_migration.json" | while read -r application; do
+            Destination_Reponame=$(echo "$application" | jq -r '.Destination_Reponame')
+            Source_Repo=$(echo "$application" | jq -r '.Source_Repo')
+            Team_Permission=$(echo "$application" | jq -r '.Team_Permission')
+            Owner=$(echo "$application" | jq -r '.Owner')
+
+            echo "Destination_Reponame $Destination_Reponame"
+            echo "Source_Repo $Source_Repo"
+            echo "Team_Permission $Team_Permission"
+            echo "Owner $Owner"    
+        
+  
+        done
+        
 gh repo create AngularApp --private
 git clone --bare https://oauth2:$ADO_TOKEN@dev.azure.com/CDEDevOps/CDEDevOps_Assets/_git/AngularApp || echo "Repo exists"
 cd AngularApp.git
