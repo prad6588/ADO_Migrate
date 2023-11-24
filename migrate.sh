c4d76663d9559482060ecd9a1f8d34840ef4bfe9
prad6588 <73686322+prad6588@users.noreply.github.com>
2023-11-24 21:33:37 +0530

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -16,7 +16,7 @@ gh extension install github/gh-ado2gh
 gh extension upgrade github/gh-ado2gh
 export GH_PAT="$GITHUB_TOKEN"
 export ADO_PAT="$ADO_TOKEN"
-git ls-remote "https://$accountname:$GITHUB_TOKEN@github.com/$Orgname/$Destination_Reponame.git
+git ls-remote "https://$accountname:$GITHUB_TOKEN@github.com/$Orgname/$Destination_Reponame.git"
 myStr="$(git ls-remote "https://$accountname:$GITHUB_TOKEN@github.com/$Orgname/$Destination_Reponame.git" &> /dev/null)";
          
 if [[ "$?" -eq 0 ]]
